614ceb64a4752ae5f4736575a0ed42328c91d98e
LiuShuhao <liushuhao@hust.edu.cn>
2025-03-12 11:52:23 +0800

    Fixed an error where .get_GRID() cannot be used when inputting certain files.

--- pysrc/auxiliary/load_file/LoadShp.py
+++ pysrc/auxiliary/load_file/LoadShp.py
@@ -2,40 +2,19 @@ import pathlib
 
 import numpy as np
 import geopandas as gpd
+from shapely.geometry import Point
+from shapely.prepared import prep
 import shapely.vectorized
 
 from pysrc.data_class.GRD import GRD
 from pysrc.auxiliary.aux_tool.MathTool import MathTool
 
 
-class LoadShpConfig:
-    def __init__(self):
-        self.__filepath = None
-
-    def set_path(self, path: pathlib.Path):
-        self.__filepath = path
-
-        return self
-
-    def get_path(self):
-        return self.__filepath
-
-
 class LoadShp:
-    def __init__(self, path: pathlib.Path = None):
-        self.configuration = LoadShpConfig()
-        if path is not None:
-            self.configuration.set_path(path)
-            self.__prepare()
+    def __init__(self, path: pathlib.Path):
+        self.__gpf = gpd.read_file(path)
 
-        self.__gpf = None
-
-    def __prepare(self):
-        shp_filepath = self.configuration.get_path()
-        self.__gpf = gpd.read_file(shp_filepath)
-        return self
-
-    def __load_mask(self, grid_space=None, identity_name: str = None):
+    def __load_mask_old(self, grid_space=None, identity_name: str = None):
         if identity_name is None:
             identity_name = "Id"
 
@@ -60,6 +39,48 @@ class LoadShp:
 
         return np.array(masks_list), lat, lon
 
+    def __load_mask(self, grid_space):
+        """
+        Generate individual global grid masks for each independent feature in the Shapefile
+
+        params：
+        shp_path: pathlib.Path
+        grid_space: int ot float, space of global grid
+
+        return:
+        list: list[np.ndarray], list of individual global grid masks
+        """
+
+        gdf = self.__gpf
+
+        gdf.to_crs(epsg=4326, inplace=True)
+        geometries = gdf.geometry.tolist()
+
+        output_width, output_height = int(round(360 / grid_space, 0)), int(round(180 / grid_space, 0)),
+
+        mask_global_list = []
+        lat, lon = MathTool.get_global_lat_lon_range(grid_space)
+
+        for idx, geom in enumerate(geometries):
+            mask_global = np.zeros((output_height, output_width), dtype=np.uint8)
+
+            g_minx, g_miny, g_maxx, g_maxy = geom.bounds
+            lat_index = np.where((lat < g_maxy) * (lat > g_miny))[0]
+            lon_index = np.where((lon < g_maxx) * (lon > g_minx))[0]
+
+            prepared_geom = prep(geom)
+
+            for i in lat_index:
+                this_lat = lat[i]
+                for j in lon_index:
+                    this_lon = lon[j]
+                    if prepared_geom.contains(Point(this_lon, this_lat)):
+                        mask_global[i, j] = 1
+
+            mask_global_list.append(mask_global)
+
+        return mask_global_list, lat, lon
+
     def __load_bound(self):
         if self.__gpf is None:
             self.__prepare()
@@ -71,7 +92,7 @@ class LoadShp:
 
     def get_GRID(self, grid_space, identity_name: str = None):
 
-        mask, lat, lon = self.__load_mask(grid_space, identity_name)
+        mask, lat, lon = self.__load_mask(grid_space)
 
         return GRD(mask, lat, lon)
 
